47b1657ee3bc3d5de6207b429f5d3e7788b043c2
olegsea <ta.alexashow@ya.ru>
2022-12-14 19:15:59 +0300

    some prettifying

--- src/io.rs
+++ src/io.rs
@@ -1,27 +1,40 @@
 use std::io;
 
 pub fn get_line() -> String {
-    let mut input_line = String::new();
+    let mut user_input = String::new();
     io::stdin()
-        .read_line(&mut input_line)
+        .read_line(&mut user_input)
         .expect("Failed to read the line!");
-    input_line
+    user_input
 }
 
 pub fn parse_i32(input_line: String) -> i32 {
-    input_line.trim().parse().expect("Not a number!")
+    // Trim the whitespace from the string
+    let trimmed = input_line.trim();
+    // Convert the trimmed string to a number
+    let parsed = trimmed.parse();
+    // If the string was not a number, panic
+    let number = parsed.expect("Not a number!");
+    // Return the parsed number
+    number
 }
 
+// parse_f32 converts a string input to a float32
+// and returns the value
 pub fn parse_f32(input_line: String) -> f32 {
     input_line.trim().parse().expect("Not a number!")
 }
 
 pub fn get_number(msg: Option<&str>) -> i32 {
+    // If there is a message, print it
     match msg {
         Some(msg) => println!("{}", msg),
         None => (),
     };
 
+    // Get a line of input from the user
     let input_line = get_line();
+
+    // Parse the input line as an integer
     parse_i32(input_line)
 }
--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,12 @@ mod io;
 type Matrix = Vec<Vec<f32>>;
 
 fn define_matrix() -> Matrix {
+    // Get the size of the matrix from the user
     let matrix_size = io::get_number(Some("Matrix size: "));
     println!("Matrix rows (in a format like \"1 2 3 4 5\"");
     let mut matrix = Vec::new();
     for _ in 0..matrix_size {
+        // Get a row from the user
         matrix.push({
             let row: Vec<f32> = io::get_line()
                 .split(" ")
@@ -20,30 +22,40 @@ fn define_matrix() -> Matrix {
 }
 
 fn calculate_determinant_ref(matrix: &Matrix) -> f32 {
+    // Matrix must be square
+    assert!(matrix.len() == matrix[0].len());
+    // Matrix must be at least 1x1
+    assert!(matrix.len() >= 1);
+
     match matrix.len() {
         1 => matrix[0][0],
-        _ => matrix[0]
-            .iter()
-            .enumerate()
-            .map(|(index, value)| {
-                (-1.0f32).powf((index % 2) as f32)
-                    * value
-                    * calculate_determinant_ref(
-                        &matrix[1..]
-                            .to_vec()
-                            .into_iter()
-                            .map(|row| {
-                                row.iter()
-                                    .enumerate()
-                                    .filter(|(column, &_)| column != &index)
-                                    .map(|(_, &value)| value)
-                                    .collect()
-                            })
-                            .map(|vec: Vec<f32>| vec)
-                            .collect::<Matrix>(),
-                    )
-            })
-            .sum(),
+        _ => {
+            // Iterate over the first row of the matrix
+            matrix[0]
+                .iter()
+                .enumerate()
+                .map(|(index, value)| {
+                    // Calculate the cofactor of the current element
+                    (-1.0f32).powf((index % 2) as f32)
+                        * value
+                        * calculate_determinant_ref(
+                            // Calculate the minor of the current element
+                            &matrix[1..]
+                                .to_vec()
+                                .into_iter()
+                                .map(|row| {
+                                    row.iter()
+                                        .enumerate()
+                                        .filter(|(column, &_)| column != &index)
+                                        .map(|(_, &value)| value)
+                                        .collect()
+                                })
+                                .map(|vec: Vec<f32>| vec)
+                                .collect::<Matrix>(),
+                        )
+                })
+                .sum()
+        }
     }
 }
 
